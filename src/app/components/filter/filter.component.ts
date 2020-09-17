a1e70c77f6c4266840e446b6912768f8dbe5b3fb
Bhumin Bhandari <bhandaribhumin@gmail.com>
2020-09-17 20:37:56 +0530

    add button active class

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -10,7 +10,9 @@ import { IFilter } from 'src/app/shared/madel';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-
+  isActivYear:string = "";
+  isActivLaunch:boolean;
+  isActivLandLaunch:boolean;
   constructor(private _spacexService: SpaceXService) { }
 
   launchYears = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
@@ -20,17 +22,19 @@ this.filterQueryParams = {} as IFilter;
   }
 
   filterAsPerLaunchYear(event){
-   console.log(event.target.value);
+   this.isActivYear = event.target.value;
    this.filterQueryParams.launchYear = event.target.value;
    this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
   }
 
   filterAsPerSuccessLaunch(value: boolean){
+    this.isActivLaunch = value;
     this.filterQueryParams.launchSuccess = value;
     this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
   }
 
   filterAsPerLandLaunch(value: boolean) {
+    this.isActivLandLaunch = value;
     this.filterQueryParams.landSuccess = value;
     this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
   }
